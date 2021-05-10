fd9425991a95384d3a86c5129116f91986db6449
TurulR3D <33686482+TurulR3D@users.noreply.github.com>
2021-05-10 10:57:13 +0200

    Full

---
+++ StudizPrototype/app/src/main/java/com/example/studizprototype/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.studizprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, homepage1.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
---
+++ StudizPrototype/app/src/main/java/com/example/studizprototype/homepage1.java
@@ -0,0 +1,106 @@
+package com.example.studizprototype;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class homepage1 extends AppCompatActivity {
+
+    GridView gridView;
+
+    String[] names = {"image1", "image2", "image3", "image4", "image5", "image6"};
+    int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5, R.drawable.image6};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage1);
+
+
+        //Find my gridView.
+        gridView = findViewById(R.id.gridView);
+
+        //Link between my UI components and the data source.
+        CustomAdapter customAdapter = new CustomAdapter(names, images, this);
+
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String selectedName = names[position];
+                int selectedImage = images[position];
+
+                startActivity(new Intent(homepage1.this, ClickedItemActivity.class).putExtra("name", selectedName).putExtra("image", selectedImage));
+
+            }
+        });
+
+    }
+
+    public class CustomAdapter extends BaseAdapter {
+        private String[] imageNames;
+        private int[] imagesPhoto;
+        private Context context;
+        private LayoutInflater layoutInflater;
+
+        public CustomAdapter(String[] imageNames, int[] imagesPhoto, Context context) {
+            this.imageNames = imageNames;
+            this.imagesPhoto = imagesPhoto;
+            this.context = context;
+            this.layoutInflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return imagesPhoto.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+
+            if(view == null) {
+                view = layoutInflater.inflate(R.layout.row_items, viewGroup,false);
+            }
+
+            //Find my text and image.
+            TextView tvName = view.findViewById(R.id.tvName);
+            ImageView imageView = view.findViewById(R.id.imageView);
+
+            //Set my text and image.
+            tvName.setText(imageNames[position]);
+            imageView.setImageResource(imagesPhoto[position]);
+
+
+            return view;
+        }
+    }
+
+}
