2009b701494fb61f70e6e4c02743310e026a3842
Daniel Hansen <71641755+Triumfvej@users.noreply.github.com>
2021-05-17 13:43:18 +0200

    lavede login skærm med studiekort godkendelse. Gjorde så man kommer fra startskærmen til login når man åbner appen og efter login bliver man smidt til homepage1.

--- StudizPrototype/app/src/main/java/com/example/studizprototype/MainActivity.java
+++ StudizPrototype/app/src/main/java/com/example/studizprototype/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, homepage1.class);
+                Intent intent = new Intent(MainActivity.this, loginScreen.class);
                 startActivity(intent);
                 finish();
             }
